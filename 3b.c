35fee13d4e154d9383cb270277b176b86210a643
MihaiCatalin120 <mihaicatalin449@gmail.com>
2025-12-17 10:45:26 +0200

    Day 3b solution

--- 3b.c
+++ 3b.c
@@ -1,49 +1,57 @@
 #include <assert.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-unsigned long getJoltage(char fileContents[]) {
-  char result[12], max;
-  int neededChars = 12, digit = 9;
-  char *maxPointer = NULL;
+long long getJoltage(char fileContents[]) {
+  char result[200];
+  int resultLen = 0, removals;
+  char *cursor = fileContents;
 
-  while (neededChars > 0) {
+  removals = strlen(fileContents) - 1 - 12;
+  // printf("Removals: %d\n", removals);
+  //
 
-    while (digit > 0) {
-      maxPointer = strchr(maxPointer == NULL ? fileContents : maxPointer + 1,
-                          digit + '0');
-      printf("Strlen %lu\n", strlen(fileContents));
-      if (maxPointer - fileContents + 1 < strlen(fileContents) - neededChars) {
-        max = *maxPointer;
-        goto found;
+  while (isdigit(*cursor) == 1) {
+    // printf("reading %c\n", *cursor);
+    if (resultLen == 0 || *cursor <= result[resultLen - 1] || removals == 0) {
+      result[resultLen++] = *cursor;
+      // printf("Pushed %c, stack size is now %d\n", *cursor, resultLen);
+    } else {
+      while (*cursor > result[resultLen - 1] && removals > 0 && resultLen > 0) {
+        // printf("Popped %c", result[resultLen - 1]);
+        resultLen--;
+        // printf(", stack size is now %d\n", result[resultLen - 1]);
+        removals--;
       }
-
-      digit--;
+      result[resultLen++] = *cursor;
+      // printf("Pushed %c, stack size is now %d\n", *cursor, resultLen);
     }
 
-    assert(digit > 0);
-
-  found:
-    result[12 - neededChars] = max;
-    neededChars--;
-    digit = 9;
+    cursor++;
   }
 
-  return strtol(result, NULL, 10);
+  result[12] = ' ';
+  printf("Result is %s\n", result);
+  return strtoll(result, NULL, 10);
 }
 
+// This implementation fails on the test_in, where inputs are very very large
 int main() {
-  FILE *f = fopen("3_in.txt", "r");
-  // FILE *f = fopen("3_test_in.txt", "r");
+  // FILE *f = fopen("3_in.txt", "r");
+  FILE *f = fopen("3_test_in.txt", "r");
   char fileContents[200];
   unsigned long total = 0;
 
   assert(f != NULL);
 
   while (fgets(fileContents, 200, f) != NULL) {
-    printf("Got %lu joltage for %s", getJoltage(fileContents), fileContents);
-    total += getJoltage(fileContents);
+    printf("Read %s", fileContents);
+    long long result = getJoltage(fileContents);
+    printf("Got %lld joltage for %s", result, fileContents);
+    total += result;
     printf("Subtotal: %lu\n", total);
   }
 
